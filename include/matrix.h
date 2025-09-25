272ea442ddc2fa9f8e9d33d40d3fe308e4d22022
RRdimetro <salter.musik@gmail.com>
2025-09-25 11:49:28 +0300

    Complete matrix library implementation with tests

---
+++ include/matrix.h
@@ -0,0 +1,21 @@
+#pragma once
+typedef struct {
+double** data;
+int rows;
+int cols;
+} Matrix;
+// Указатель на двумерный массив
+// Количество строк
+// Количество столбцов
+// Базовые функции
+Matrix create_matrix(int rows, int cols);
+void free_matrix(Matrix m);
+Matrix matrix_add(Matrix a, Matrix b);
+Matrix matrix_multiply(Matrix a, Matrix b);
+Matrix matrix_transpose(Matrix m);
+// Вспомогательные функции
+void print_matrix(Matrix m);
+Matrix matrix_from_array(double* data, int rows, int cols);
+
+// ИНДИВИДУАЛЬНОЕ ЗАДАНИЕ: сумма элементов матрицы
+double matrix_sum(Matrix m);
